b36ac2b5f64bdfe2f481cc7e0a6edae8583221cc
ReemOssamaAhmedhelmy <reemhelmy28@gmail.com>
2026-07-31 01:33:33 +0300

    step 3 to add car

--- app/Enums/DrivetrainType.php
+++ app/Enums/DrivetrainType.php
@@ -4,11 +4,11 @@ namespace App\Enums;
 
 enum DrivetrainType: string
 {
-    case FWD = 'fwd';   // Front Wheel Drive
+    case FWD = 'fwd';   // Front Wheel Drive //
 
-    case RWD = 'rwd';   // Rear Wheel Drive
+    case RWD = 'rwd';   // Rear Wheel Drive  //
 
-    case AWD = 'awd';   // All Wheel Drive
+    case AWD = 'awd';   // All Wheel Drive   //
 
-    case FOUR_WD = '4wd'; // Four Wheel Drive
+    case FOUR_WD = '4wd'; // Four Wheel Drive //
 }
